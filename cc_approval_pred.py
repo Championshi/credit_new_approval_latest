b0ccebd69a31e502d0be481dcfcad54c484af47c
unknown <you@example.com>
2024-12-14 15:12:21 +0530

    first commit

--- cc_approval_pred.py
+++ cc_approval_pred.py
@@ -1,23 +1,23 @@
-import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, MinMaxScaler, OrdinalEncoder
-from sklearn.ensemble import GradientBoostingClassifier
-from imblearn.over_sampling import SMOTE
+# import numpy as np
+# import pandas as pd
+# from sklearn.model_selection import train_test_split
+# from sklearn.base import BaseEstimator, TransformerMixin
+# from sklearn.pipeline import Pipeline
+# from sklearn.compose import ColumnTransformer
+# from sklearn.preprocessing import OneHotEncoder, MinMaxScaler, OrdinalEncoder
+# from sklearn.ensemble import GradientBoostingClassifier
+# from imblearn.over_sampling import SMOTE
 
-# from secret import access_key, secret_access_key
-import joblib
-import streamlit as st
-import boto3
-import tempfile
-import json
-import requests
-from streamlit_lottie import st_lottie_spinner
-import logging
-from botocore.exceptions import ClientError
+# # from secret import access_key, secret_access_key
+# import joblib
+# import streamlit as st
+# import boto3
+# import tempfile
+# import json
+# import requests
+# from streamlit_lottie import st_lottie_spinner
+# import logging
+# from botocore.exceptions import ClientError
 
 
 # train_original = pd.read_csv(
